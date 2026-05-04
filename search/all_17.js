1a9da1d4fc1851406d0e13ce0cef0c6c19125575
anyc <dev@kicherer.org>
2026-05-04 15:06:36 +0000

    Deploying to gh-pages from @ anyc/libcoapy@e4b12b3369c6b8b47289c328e62dfd59215b443c 🚀

--- search/all_17.js
+++ search/all_17.js
@@ -2,14 +2,15 @@ var searchData=
 [
   ['wait_0',['wait',['../classlibcoapy_1_1CoapObserver_1_1CoapObserver.html#a5a7a4e9012ab53d15134323a60d378cb',1,'libcoapy::CoapObserver::CoapObserver']]],
   ['waiting_1',['waiting',['../classlibcoapy_1_1CoapObserver_1_1CoapObserverMultiplier.html#a9cf89adf60f296ddf1289d8d1e56f14c',1,'libcoapy::CoapObserver::CoapObserverMultiplier']]],
-  ['websockets_20support_2',['websockets support',['https://libcoap.net/doc/reference/4.3.5/group__ws.html',1,'WebSockets Support'],['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html',1,'WebSockets Support']]],
-  ['wrapper_5fpayload_5fcb_3',['wrapper_payload_cb',['../classlibcoapy_1_1CoapPDU_1_1CoapPDU.html#a27270804ca74db1c11dca7025e532542',1,'libcoapy::CoapPDU::CoapPDU']]],
-  ['wrapper_5fpayload_5frelease_5fcb_4',['wrapper_payload_release_cb',['../classlibcoapy_1_1CoapPDU_1_1CoapPDU.html#aeb59d3b3891e756e7d0e4fbcbf2a8475',1,'libcoapy::CoapPDU::CoapPDU']]],
-  ['writefds_5',['writefds',['https://libcoap.net/doc/reference/4.3.5/structcoap__context__t.html#aaf287872a1c5c4171fae69b58b8d978c',1,'coap_context_t']]],
-  ['ws_5fop_5fbinary_6',['WS_OP_BINARY',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aada5aba44db6244f0a086f5232a99902f',1,]]],
-  ['ws_5fop_5fclose_7',['WS_OP_CLOSE',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aa852899520a52ddf1a78d76102a069c4c',1,]]],
-  ['ws_5fop_5fcont_8',['WS_OP_CONT',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aafe189c53d1aa2a16f80d5bb6105f4e2d',1,]]],
-  ['ws_5fop_5fping_9',['WS_OP_PING',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aad7199aaae85d452b11b8950fa1d385bf',1,]]],
-  ['ws_5fop_5fpong_10',['WS_OP_PONG',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aacf785a72aad93b28698046ac776531d2',1,]]],
-  ['ws_5fop_5ftext_11',['WS_OP_TEXT',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aa5a3a2d1e8cfb21ed7a233048a0e77f43',1,]]]
+  ['weakref_2',['weakref',['../classlibcoapy_1_1CoapSession_1_1CoapSession.html#aa11266575aed4eae00d966ec1e062508',1,'libcoapy::CoapSession::CoapSession']]],
+  ['websockets_20support_3',['websockets support',['https://libcoap.net/doc/reference/4.3.5/group__ws.html',1,'WebSockets Support'],['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html',1,'WebSockets Support']]],
+  ['wrapper_5fpayload_5fcb_4',['wrapper_payload_cb',['../classlibcoapy_1_1CoapPDU_1_1CoapPDU.html#a27270804ca74db1c11dca7025e532542',1,'libcoapy::CoapPDU::CoapPDU']]],
+  ['wrapper_5fpayload_5frelease_5fcb_5',['wrapper_payload_release_cb',['../classlibcoapy_1_1CoapPDU_1_1CoapPDU.html#aeb59d3b3891e756e7d0e4fbcbf2a8475',1,'libcoapy::CoapPDU::CoapPDU']]],
+  ['writefds_6',['writefds',['https://libcoap.net/doc/reference/4.3.5/structcoap__context__t.html#aaf287872a1c5c4171fae69b58b8d978c',1,'coap_context_t']]],
+  ['ws_5fop_5fbinary_7',['WS_OP_BINARY',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aada5aba44db6244f0a086f5232a99902f',1,]]],
+  ['ws_5fop_5fclose_8',['WS_OP_CLOSE',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aa852899520a52ddf1a78d76102a069c4c',1,]]],
+  ['ws_5fop_5fcont_9',['WS_OP_CONT',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aafe189c53d1aa2a16f80d5bb6105f4e2d',1,]]],
+  ['ws_5fop_5fping_10',['WS_OP_PING',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aad7199aaae85d452b11b8950fa1d385bf',1,]]],
+  ['ws_5fop_5fpong_11',['WS_OP_PONG',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aacf785a72aad93b28698046ac776531d2',1,]]],
+  ['ws_5fop_5ftext_12',['WS_OP_TEXT',['https://libcoap.net/doc/reference/4.3.5/group__ws__internal.html#gga582505eeb3cc29e76dcb08bc9bfd0c0aa5a3a2d1e8cfb21ed7a233048a0e77f43',1,]]]
 ];
